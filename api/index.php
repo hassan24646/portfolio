1ff6b7b233e5a425893697a554c4bbd41e28fe34
hassan24646 <hassan.abbas2233@gmail.com>
2026-08-17 16:47:17 +0500

    Fix Vercel serverless deployment

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp');
+
+$app->handleRequest(Illuminate\Http\Request::capture());
