d22771d49e23af0a880eb848834aefae976539e6
hassan24646 <hassan.abbas2233@gmail.com>
2026-08-20 16:04:48 +0500

    Hardcode SESSION_DRIVER to cookie to fix ArgumentCountError

--- api/index.php
+++ api/index.php
@@ -4,10 +4,19 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$_ENV['APP_DEBUG'] = true;
-$_SERVER['APP_DEBUG'] = true;
-putenv('APP_DEBUG=true');
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/framework/views';
+$_ENV['CACHE_DRIVER'] = 'array';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['LOG_CHANNEL'] = 'stderr';
 
+foreach ($_ENV as $key => $value) {
+    $_SERVER[$key] = $value;
+}
 
 try {
     $app = require_once __DIR__.'/../bootstrap/app.php';
