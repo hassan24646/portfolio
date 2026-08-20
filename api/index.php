7a6803f9b68be71cf407c30005133fe58a89fdd3
hassan24646 <hassan.abbas2233@gmail.com>
2026-08-20 16:13:36 +0500

    Add putenv to fully override Vercel environment variables

--- api/index.php
+++ api/index.php
@@ -4,18 +4,22 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
-$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
-$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
-$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/framework/views';
-$_ENV['CACHE_DRIVER'] = 'array';
-$_ENV['SESSION_DRIVER'] = 'cookie';
-$_ENV['LOG_CHANNEL'] = 'stderr';
+$envVars = [
+    'APP_CONFIG_CACHE' => '/tmp/config.php',
+    'APP_EVENTS_CACHE' => '/tmp/events.php',
+    'APP_PACKAGES_CACHE' => '/tmp/packages.php',
+    'APP_ROUTES_CACHE' => '/tmp/routes.php',
+    'APP_SERVICES_CACHE' => '/tmp/services.php',
+    'VIEW_COMPILED_PATH' => '/tmp/framework/views',
+    'CACHE_DRIVER' => 'array',
+    'SESSION_DRIVER' => 'cookie',
+    'LOG_CHANNEL' => 'stderr',
+];
 
-foreach ($_ENV as $key => $value) {
+foreach ($envVars as $key => $value) {
+    $_ENV[$key] = $value;
     $_SERVER[$key] = $value;
+    putenv("{$key}={$value}");
 }
 
 try {
